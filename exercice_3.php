54c8e3f90a8ba63a908faa39c8e606529d4859a9
Lucas Borter <lucas.borter@ceff.ch>
2023-08-21 13:34:52 +0200

    PHP 3.1.4 - Exercice 3

---
+++ exercice_3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Exercice 3</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+            <?php
+            $nom = "Borter";
+            $prenom = "Lucas";
+            $profession = "Informaticien";
+
+            echo "<b>$nom, $prenom</b>, $profession<br/>" ;
+            echo "<b>".$nom.", ".$prenom."</b>, ".$profession."<br/>" ;
+            ?>
+    </body>
+</html>
