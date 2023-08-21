3c79f71c1d4fb14ba243d4f4c428282382a109ed
Lucas Borter <lucas.borter@ceff.ch>
2023-08-21 13:47:56 +0200

    PHP 3.1.4 - Exercice 4

---
+++ exercice_4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Exercice -</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+    $pi = 3.1415;
+    $rayon = 4;
+    $res = $pi*$rayon**2;
+
+    echo $pi."*".$rayon."^2"."=".round($res,2)
+    ?>
+</body>
+</html>
