4d40fb142b129ddcbda4a6334cbf565537ec6c05
Lucas Borter <lucas.borter@ceff.ch>
2023-08-21 11:53:36 +0200

    PHP 3.1.2 - Exercice 2

---
+++ exercice_2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercice 2</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+    echo "<table border =\"2\">"; // code contains the escape character "\" (backslash)
+        echo "<tr>";
+            echo "<td>Nom</td>";
+            echo "<td>Prénom</td>";
+        echo "</tr>";
+        echo "<tr>";
+            echo "<td>Lerdorf</td>";
+            echo "<td>Rasmus</td>";
+        echo "</tr>";
+        echo "<tr>";
+            echo "<td>Computer</td>";
+            echo "<td>Jules</td>";
+        echo "</tr>";
+    echo "</table>";
+    ?>
+</body>
+</html>
