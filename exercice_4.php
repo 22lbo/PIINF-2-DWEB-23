3f24ea19b3d6cf476344314e10435abca381f3fd
Lucas Borter <lucas.borter@ceff.ch>
2023-08-21 15:19:47 +0200

    PHP 3.1.4 - Exercice 4

--- exercice_4.php
+++ exercice_4.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Exercice -</title>
+    <title>Exercice 4</title>
     <meta charset="UTF-8">
 </head>
 <body>
     <?php
-    $pi = 3.1415;
+    define("pi", 3.1415);
     $rayon = 4;
-    $res = $pi*$rayon**2;
+    $diametre = $rayon*2;
+    $circonference  = pi*$diametre;
+    $surface = pi*$rayon**2;
 
-    echo $pi."*".$rayon."^2"."=".round($res,2)
+    echo "Le diamètre du cercle : ".$rayon."*2"."=".round($diametre,2);
+    echo "<br>";
+    echo "La circonférence du cercle : ".$rayon."*2*".pi."=".round($circonference,2);
+    echo "<br>";
+    echo "La surface du cercle : ".pi."*".$rayon."^2"."=".round($surface,2);
     ?>
 </body>
 </html>
