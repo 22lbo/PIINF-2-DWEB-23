ad09486737a0ced604c6ef3b28e330c0a573392e
Lucas Borter <lucas.borter@ceff.ch>
2023-08-21 14:37:31 +0200

    PHP 3.1.6 - Exercice 5

---
+++ exercice_5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Exercice 5</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+    $seconde = date("s");
+
+    if ($seconde % 2) {
+        echo $seconde." est impaire";
+    }
+    else {
+        echo $seconde." est paire";
+    }
+    ?>
+</body>
+</html>
