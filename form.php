7a1a1d07afa5fbe7a4dc785cc455a9a9e825d11e
Lucas Borter <lucas.borter@ceff.ch>
2023-08-22 13:51:01 +0200

    PHP - Exercice de revision sur les formulaires

---
+++ form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Form Page</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+        $tabFeedback = array(1 => "Insuffisant", 2 => "Suffisant", 3 => "Bien", 4 => "Très bien");
+
+        echo "<form action='result.php' method='post'>"; //the get method doesn't post anything on the results page
+            echo "<input type='text' name='name'> Nom d'utilisateur<br>";
+            echo "<input type='email' name='email'> E-mail<br>";
+
+            echo "<br>";
+
+            echo "<select name='feedback'><br>";
+                echo "<option value='1'>".$tabFeedback[1]."</option>";
+                echo "<option value='2'>".$tabFeedback[2]."</option>";
+                echo "<option value='3'>".$tabFeedback[3]."</option>";
+                echo "<option value='4'>".$tabFeedback[4]."</option>";
+            echo "</select><br>";
+
+            echo "<br>";
+            echo "<textarea>Message</textarea><br>";
+            echo "<br>";
+
+            echo "<input type='radio' name='genre' value='H'>Homme<br>";
+            echo "<input type='radio' name='genre' value='F'>Femme<br>";
+
+            echo "<br>";
+
+            echo "<input type='checkbox' name='newsletter' value='1'>S'inscrire à la newsletter<br>";
+
+            echo "<br>";
+
+            echo "<input type='submit' name='send' value='Submit'>";
+        echo "</form>";
+    ?>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Results Page</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+    echo "<pre>";
+        print_r($_POST);
+    echo "<pre>";
+?>
+</body>
+</html>
