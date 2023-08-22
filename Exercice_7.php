ade299abe75723d5aa28d2060563edb9893d8898
Lucas Borter <lucas.borter@ceff.ch>
2023-08-22 08:25:41 +0200

    PHP 3.1.7 - Exercice 7

--- Exercice_7.php
+++ Exercice_7.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Exercice 5</title>
+    <title>Exercice 7</title>
     <meta charset="UTF-8">
 </head>
 <body>
     <?php
-    /*echo "<h1>Table de multiplications</h1>";*/
+    echo "<h1>Table de multiplication</h1>";
 
     echo "<table border =\"2\">";
-    echo "<tr>";
-    for($i=1; $i<13; $i++) {
-        echo "<th>$i</th>";
-        echo "<tr>";
-        for($j=1; $j<13; $j++) {
-            echo "<td>".$i*$j."</td>";
-        }
-        echo "</tr>";
-    }
-    echo "</tr>";
+        echo "<thead bgcolor =\"lightgray\">";
+            echo "<tr>";
+                for($i=0; $i<13; $i++) {
+                    echo "<th>$i</th>";
+                }
+            echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+                for($j=2; $j<13; $j++) {
+                    echo "<tr>";
+                    echo "<th bgcolor =\"lightgray\">$j</th>";
+
+                    for($k=1; $k<13; $k++) {
+                        echo "<td>".$j*$k."</td>";
+                    }
+                    echo "</tr>";
+                }
+        echo "</tbody>";
     echo "</table>";
     ?>
 </body>
