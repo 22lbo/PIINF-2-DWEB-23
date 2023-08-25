3970c405a8f36643637fc130c0d4b80e2c36ee93
Lucas Borter <lucas.borter@ceff.ch>
2023-08-25 10:28:04 +0200

    PHP - Exercice formulaire Swiss

--- form_swiss.php
+++ form_swiss.php
@@ -1,48 +1,83 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Results Page</title>
+    <title>Formulaire "Swiss"</title>
     <meta charset="UTF-8">
+    <style>
+        html {
+            margin:0px;
+            padding:0px;
+            width:auto;
+        }
+        body {
+            width:500px;
+            margin-left: auto;
+            margin-right:auto;
+        }
+        .text {
+            display:inline;
+            font-weight:bold;
+        }
+        .boxes{
+            margin-left:20px;
+        }
+        .checkbox {
+            margin-top: 30px;
+        }
+        .send_button{
+            color:
+            float:center;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     <?php
     define("depart", "Genève");
-    $destinations = array("Adélaïde","Alicante","Athenes","Atlanta","Buenos Aires", "Bâle");
+    $destinations = array("ADL"=>"Adélaïde","ALC"=>"Alicante","ATH"=>"Athenes","ATL"=>"Atlanta","BUE"=>"Buenos Aires","BSL"=>"Bâle","BCN"=>"Barcelone","BKK"=>"Bangkok",
+        "CPH"=>"Copenhague","CLE"=>"Cleveland","CPT"=>"Le Cap","DBV"=>"Dubrovnik","DUB"=>"Dublin","DEN"=>"Denver","EDI"=>"Edimbourg","FLR"=>"Florence",
+        "FUE"=>"Fuerteventura","GOT"=>"Goteborg","GVA"=>"Genève","GLA"=>"Glasgow","HAM"=>"Hambourg","HEL"=>"Helsinki","HNL"=>"Honolulu","HKG"=>"Hong Kong","IBZ"=>"Ibiza",
+        "IND"=>"Indianapolis","INN"=>"Innsbruck","JNB"=>"Johannesburg","KWI"=>"Kuwait","IEV"=>"Kiev","LON"=>"Londres","LAX"=>"Los Angeles","LIS"=>"Lisbonne",
+        "MOW"=>"Moscou","MLA"=>"Malte","MUC"=>"Munich","NYC"=>"New York","NBO"=>"Nairobi","NAS"=>"Nassau","ORL"=>"Orlando","OSA"=>"Osaka","ODS"=>"Odessa","PAR"=>"Paris",
+        "PRG"=>"Prague","YQB"=>"Quebec","ROM"=>"Rome","RIO"=>"Rio de Janeiro","REK"=>"Reykjavik","SFO"=>"San Francisco","SOF"=>"Sofia","SYD"=>"Sydney","TCI"=>"Ténérife",
+        "TYO"=>"Tokyo","YTO"=>"Toronto","UME"=>"Umea","VCE"=>"Venise","YVR"=>"Vancouver","WAM"=>"Varsovie","WAS"=>"Washington","WLG"=>"Wellington","WRO"=>"Wroclaw",
+        "SIA"=>"Xian","XMN"=>"Xiamen","YAO"=>"Yaoundé","ZHR"=>"Zurich","ZTH"=>"Zakynthos");
     $dateD = date("Y-m-d");
     $dateR = date("Y-m-d", strtotime("tomorrow"));
-    echo "<img src='C:\wamp64\www\PIINF-2-DWEB-23\logo-swiss-2x.png' alt='SWISS airline logo'>";
+    echo "<img src='.\logo-swiss-2x.png' alt='SWISS airline logo' height='100px'>";
     echo "<h2>Réservez votre vol</h2>";
     echo "<form action='result.php' method='post'>";
-        echo "De";
-        echo "<select>";
-            echo "<option name='depart'>".depart."</option>";
+        echo "<div class='text'>De</div>";
+        echo "<select class='boxes' name='depart'>";
+            echo "<option >".depart."</option>";
         echo "</select>";
-        echo "À";
-        echo "<select>";
-            echo "<option name='destination'>".$destinations[0]."</option>";
-            echo "<option name='destination'>".$destinations[1]."</option>";
-            echo "<option name='destination'>".$destinations[2]."</option>";
-            echo "<option name='destination'>".$destinations[3]."</option>";
-            echo "<option name='destination'>".$destinations[4]."</option>";
-            echo "<option name='destination'>".$destinations[5]."</option>";
+        echo "<div class='text' class='secondRow'>À</div>";
+        echo "<select class='boxes' name='destination'>";
+            foreach($destinations AS $key => $value) {
+                echo "<option value='$key'>".$value."</option>";
+            }
         echo "</select>";
 
         echo "<br>";
-        echo "Vol Aller";
-        echo "<input type='date' name='dateD' value=$dateD>";
-        echo "Vol Retour";
-        echo "<input type='date' name='dateR' value=$dateR>";
-        echo "<br>";
+        echo "<div class='text'>Vol Aller</div>";
+        echo "<input class='boxes' type='date' name='date_depart' value=$dateD>";
+        echo "<div class='text'class='secondRow'>Vol Retour</div>";
+        echo "<input class='boxes' type='date' name='date_arrivee' value=$dateR>";
 
-        echo "Adultes <input type='text' value='1'>";
+        $categories = array("cat_1" => "Adultes","cat_2" => "Enfants","cat_3" => "Bébés");
+        foreach($categories AS $key => $value) {
+            echo "<br>";
+            $passValue = 0;
+            if ($key == "cat_1") {
+                $passValue = 1;
+            }
+            echo "<div class='text'>$value</div> <input class='boxes' type='text' name='cat_1' value='$passValue'>";
+        }
+
         echo "<br>";
-        echo "Enfants <input type='text' value='0'>";
+        echo "<input class='checkbox' type='checkbox' name='reserver_siege' value='1'>Réserver votre siège ?";
         echo "<br>";
-        echo "Bébés <input type='text' value='0'>";
-    echo "<br>";
-        echo "<input type='checkbox'>Réserver votre siège ?";
-    echo "<br>";
-        echo "<input type='submit' value='Rechercher votre Vol'>";
+        echo "<input class='send_button' type='submit' name='rechercher' value='Rechercher votre Vol'>";
     echo "</form>";
     ?>
 </body>
