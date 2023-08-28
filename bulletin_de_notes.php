1cca35803b5c0b55a98a1b4797d976d600edd895
Lucas Borter <lucas.borter@ceff.ch>
2023-08-28 16:29:22 +0200

    PHP - Bulletin de notes

--- bulletin_de_notes.php
+++ bulletin_de_notes.php
@@ -29,29 +29,28 @@
 <body>
     <?php
     //tableau des titres des modules theoriques
-    $tab_modules['nom'] = array("106" => "Interroger, traiter et assurer la maintenance des bases de données","114" => "Mettre en œuvre des systèmes de codification, de compression et d’encryptage",
+    $tab['modules']['nom'] = array("106" => "Interroger, traiter et assurer la maintenance des bases de données","114" => "Mettre en œuvre des systèmes de codification, de compression et d’encryptage",
         "117" => "Mettre en place l’infrastructure informatique et réseau d’une petite entreprise","122" => "Automatiser des procédures à l’aide de scripts","162" => "Analyser et modéliser des données",
         "164" => "Créer des bases de données et y insérer des données","231" => "Appliquer la protection et la sécurité des données","319" => "Concevoir et implémenter des applications","320" => "Programmer orienté objet",
         "322" => "Concevoir et implémenter des interfaces utilisateur","346" => "Concevoir et réaliser des solutions cloud","431" => "Exécuter des mandats de manière autonome dans son propre environnement professionnel");
 
     //tableau des notes de CCO des modules theoriques
-    $tab_modules['notes'] = array('106' => '6', '117' => '5.5', '122' => '5', '162' => '5', '164' => '5.5', '231' => '5.5', '319' => '4', '431' => '4.5');
-
-    //calcule la moyenne totale des modules et l'introduit dans la variable
-    foreach($tab_modules['notes'] AS $key => $val)
-        $modules_moyenne = array_sum($tab_modules['notes']) / sizeof($tab_modules['notes']);
-
+    $tab['modules']['notes'] = array('106' => '6', '117' => '5.5', '122' => '5', '162' => '5', '164' => '5.5', '231' => '5.5', '319' => '4', '431' => '4.5');
 
     //tableau des titres des CIE
-    $tab_cie['nom'] = array("123" => "Activer les services d´un serveur","187" => "Mettre en service un poste de travail ICT avec le système d’exploitation",
+    $tab['cie']['nom'] = array("123" => "Activer les services d´un serveur","187" => "Mettre en service un poste de travail ICT avec le système d’exploitation",
         "216" => "Intégrer les terminaux IoE dans une plateforme existante","293" => "Créer et publier un site Web");
 
     //tableau des notes de CCO des CIE
-    $tab_cie['notes'] = array('123' => '5.5', '187' => '5.5', '216' => '5.5', '293' => '6');
+    $tab['cie']['notes'] = array('123' => '5.5', '187' => '5.5', '216' => '5.5', '293' => '6');
+
+    //calcule la moyenne totale des modules et l'introduit dans la variable
+    foreach($tab['modules']['notes'] AS $key => $val)
+        $modules_moyenne = array_sum($tab['modules']['notes']) / sizeof($tab['modules']['notes']);
 
     //calcule la moyenne totale des CIE et l'introduit dans la variable
-    foreach($tab_cie['notes'] AS $key => $val)
-        $cie_moyenne = array_sum($tab_cie['notes'])/ sizeof($tab_cie['notes']);
+    foreach($tab['cie']['notes'] AS $key => $val)
+        $cie_moyenne = array_sum($tab['cie']['notes'])/ sizeof($tab['cie']['notes']);
 
 
     //contient une moyenne calculée depuis la moyenne des modules et la moyenne des CIE
@@ -88,12 +87,12 @@
             echo "<th>Modules de compétences en informatique</th>";
             echo "<th>Notes</th>";
         echo "</tr>";
-        abc($tab_modules['notes'], $key, $val, $tab_modules['nom']);
+        abc($tab['modules']['notes'], $key, $val, $tab['modules']['nom']);
 
         echo "<tr>";
             echo "<th>Cours Interentreprises</th>";
         echo "</tr>";
-        abc($tab_cie['notes'], $key, $val, $tab_cie['nom']);
+        abc($tab['cie']['notes'], $key, $val, $tab['cie']['nom']);
 
         echo "<tr>";
             echo "<th>Compentences en informatique</th>";
