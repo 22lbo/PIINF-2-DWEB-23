ce68b132188e811fa0f368ef9e3f6a4399a8abd5
Lucas Borter <lucas.borter@ceff.ch>
2023-09-04 16:29:52 +0200

    PHP -Bulletin de notes
    Ajout de tous les modules de la formation

--- bulletin_de_notes_v2.php
+++ bulletin_de_notes_v2.php
@@ -40,45 +40,132 @@
 <?php
     $tab['ci']['desc'] = 'Competences en informatique';
     $tab['ci']['ponderation'] =80;
-    $tab['ci']['modules'][106]['nom'] = 'Interroger, traiter et assurer la maintenance des bases de données';
-    $tab['ci']['modules'][106]['note'] = '6';
+    $tab['ci']['modules'][106]['nom'] = "Interroger, traiter et assurer la maintenance des bases de données";
+    $tab['ci']['modules'][106]['note'] = 6;
     $tab['ci']['modules'][106]['date'] = '2023-06-23';
+    $tab['ci']['modules'][114]['nom'] = "Mettre en œuvre des systèmes de codification, de compression et d’encryptage";
+    $tab['ci']['modules'][114]['note'] = null;
+    $tab['ci']['modules'][114]['date'] = '';
     $tab['ci']['modules'][117]['nom'] = "Mettre en place l’infrastructure informatique et réseau d’une petite entreprise";
-    $tab['ci']['modules'][117]['note'] = '5.5';
+    $tab['ci']['modules'][117]['note'] = 5.5;
     $tab['ci']['modules'][117]['date'] = '2023-01-19';
     $tab['ci']['modules'][122]['nom'] = "Automatiser des procédures à l’aide de scripts";
-    $tab['ci']['modules'][122]['note'] = '5';
+    $tab['ci']['modules'][122]['note'] = 5;
     $tab['ci']['modules'][122]['date'] = '2023-06-20';
     $tab['ci']['modules'][162]['nom'] = "Analyser et modéliser des données";
-    $tab['ci']['modules'][162]['note'] = '5';
+    $tab['ci']['modules'][162]['note'] = 5;
     $tab['ci']['modules'][162]['date'] = '2023-01-18';
     $tab['ci']['modules'][164]['nom'] = "Créer des bases de données et y insérer des données";
-    $tab['ci']['modules'][164]['note'] = '5.5';
+    $tab['ci']['modules'][164]['note'] = 5.5;
     $tab['ci']['modules'][164]['date'] = '2023-04-05';
+    $tab['ci']['modules'][165]['nom'] = "Utiliser des bases de données NoSQL";
+    $tab['ci']['modules'][165]['note'] = null;
+    $tab['ci']['modules'][165]['date'] = '';
+    $tab['ci']['modules'][183]['nom'] = "Implémenter la sécurité d'une application";
+    $tab['ci']['modules'][183]['note'] = null;
+    $tab['ci']['modules'][183]['date'] = '';
     $tab['ci']['modules'][231]['nom'] = "Appliquer la protection et la sécurité des données";
-    $tab['ci']['modules'][231]['note'] = '5.5';
+    $tab['ci']['modules'][231]['note'] = 5.5;
     $tab['ci']['modules'][231]['date'] = '2023-06-19';
+    $tab['ci']['modules'][241]['nom'] = "Initialiser des solutions ICT innovantes";
+    $tab['ci']['modules'][241]['note'] = null;
+    $tab['ci']['modules'][241]['date'] = '';
+    $tab['ci']['modules'][245]['nom'] = "Mettre en œuvre des solutions ICT innovantes";
+    $tab['ci']['modules'][245]['note'] = null;
+    $tab['ci']['modules'][245]['date'] = '';
+    $tab['ci']['modules'][254]['nom'] = "Décrire des processus métier dans son propre environnement professionnel";
+    $tab['ci']['modules'][254]['note'] = null;
+    $tab['ci']['modules'][254]['date'] = '';
+    $tab['ci']['modules'][306]['nom'] = "Réaliser de petits projets dans son propre environnement professionnel";
+    $tab['ci']['modules'][306]['note'] = null;
+    $tab['ci']['modules'][306]['date'] = '';
     $tab['ci']['modules'][319]['nom'] = "Concevoir et implémenter des applications";
-    $tab['ci']['modules'][319]['note'] = '4';
+    $tab['ci']['modules'][319]['note'] = 4;
     $tab['ci']['modules'][319]['date'] = '2023-01-20';
+    $tab['ci']['modules'][320]['nom'] = "Programmer orienté objet";
+    $tab['ci']['modules'][320]['note'] = null;
+    $tab['ci']['modules'][320]['date'] = '';
+    $tab['ci']['modules'][321]['nom'] = "Programmer des systèmes distribués";
+    $tab['ci']['modules'][321]['note'] = null;
+    $tab['ci']['modules'][321]['date'] = '';
+    $tab['ci']['modules'][322]['nom'] = "Concevoir et implémenter des interfaces utilisateur";
+    $tab['ci']['modules'][322]['note'] = null;
+    $tab['ci']['modules'][322]['date'] = '';
+    $tab['ci']['modules'][323]['nom'] = "Programmer de manière fonctionnelle";
+    $tab['ci']['modules'][323]['note'] = null;
+    $tab['ci']['modules'][323]['date'] = '';
+    $tab['ci']['modules'][324]['nom'] = "Prendre en charge des processus DevOps avec des outils logiciels";
+    $tab['ci']['modules'][324]['note'] = null;
+    $tab['ci']['modules'][324]['date'] = '';
+    $tab['ci']['modules'][346]['nom'] = "Concevoir et réaliser des solutions cloud";
+    $tab['ci']['modules'][346]['note'] = null;
+    $tab['ci']['modules'][346]['date'] = '';
+    $tab['ci']['modules'][347]['nom'] = "Utiliser un service avec des conteneurs";
+    $tab['ci']['modules'][347]['note'] = null;
+    $tab['ci']['modules'][347]['date'] = '';
+    $tab['ci']['modules'][426]['nom'] = "Développer un logiciel avec des méthodes agiles";
+    $tab['ci']['modules'][426]['note'] = null;
+    $tab['ci']['modules'][426]['date'] = '';
     $tab['ci']['modules'][431]['nom'] = "Exécuter des mandats de manière autonome dans son propre environnement professionnel";
-    $tab['ci']['modules'][431]['note'] = '4.5';
+    $tab['ci']['modules'][431]['note'] = 4.5;
     $tab['ci']['modules'][431]['date'] = '2023-01-16';
+    $tab['ci']['modules'][450]['nom'] = "Tester des applications";
+    $tab['ci']['modules'][450]['note'] = null;
+    $tab['ci']['modules'][450]['date'] = '';
 
     $tab['cie']['desc'] = 'Cours Interentreprise';
     $tab['cie']['ponderation'] =20;
-    $tab['cie']['modules'][123]['nom'] = 'Activer les services d´un serveur';
-    $tab['cie']['modules'][123]['note'] = '5.5';
+    $tab['cie']['modules'][107]['nom'] = "Mettre en œuvre des solutions ICT avec la technologie blockchain";
+    $tab['cie']['modules'][107]['note'] = null;
+    $tab['cie']['modules'][107]['date'] = '';
+    $tab['cie']['modules'][109]['nom'] = "Exploiter et surveiller des services dans le cloud public";
+    $tab['cie']['modules'][109]['note'] = null;
+    $tab['cie']['modules'][109]['date'] = '';
+    $tab['cie']['modules'][110]['nom'] = "Analyser et représenter des données avec des outils";
+    $tab['cie']['modules'][110]['note'] = null;
+    $tab['cie']['modules'][110]['date'] = '';
+    $tab['cie']['modules'][123]['nom'] = "Activer les services d'un serveur";
+    $tab['cie']['modules'][123]['note'] = 5.5;
     $tab['cie']['modules'][123]['date'] = '2023-05-08';
-    $tab['cie']['modules'][187]['nom'] = 'Mettre en service un poste de travail ICT avec le système d’exploitation';
-    $tab['cie']['modules'][187]['note'] = '5.5';
+    $tab['cie']['modules'][185]['nom'] = "Analyser et implémenter des mesures visant à assurer la sécurité informatique des PME";
+    $tab['cie']['modules'][185]['note'] = null;
+    $tab['cie']['modules'][185]['date'] = '';
+    $tab['cie']['modules'][187]['nom'] = "Mettre en service un poste de travail ICT avec le système d’exploitation";
+    $tab['cie']['modules'][187]['note'] = 5.5;
     $tab['cie']['modules'][187]['date'] = '2022-09-16';
-    $tab['cie']['modules'][216]['nom'] = 'Intégrer les terminaux IoE dans une plateforme existante';
-    $tab['cie']['modules'][216]['note'] = '5.5';
+    $tab['cie']['modules'][190]['nom'] = "Mettre en place et exploiter une plateforme de virtualisation";
+    $tab['cie']['modules'][190]['note'] = null;
+    $tab['cie']['modules'][190]['date'] = '';
+    $tab['cie']['modules'][210]['nom'] = "Utiliser un cloud public pour des applications";
+    $tab['cie']['modules'][210]['note'] = null;
+    $tab['cie']['modules'][210]['date'] = '';
+    $tab['cie']['modules'][216]['nom'] = "Intégrer les terminaux IoE dans une plateforme existante";
+    $tab['cie']['modules'][216]['note'] = 5.5;
     $tab['cie']['modules'][216]['date'] = '2023-03-16';
-    $tab['cie']['modules'][293]['nom'] = 'Créer et publier un site Web';
-    $tab['cie']['modules'][293]['note'] = '6';
+    $tab['cie']['modules'][217]['nom'] = "Concevoir, planifier et mettre en place un service pour l’IoE";
+    $tab['cie']['modules'][217]['note'] = null;
+    $tab['cie']['modules'][217]['date'] = '';
+    $tab['cie']['modules'][223]['nom'] = "Réaliser des applications multi-utilisateurs orientées objets";
+    $tab['cie']['modules'][223]['note'] = null;
+    $tab['cie']['modules'][223]['date'] = '';
+    $tab['cie']['modules'][248]['nom'] = "Réaliser des solutions ICT avec des technologies actuelles";
+    $tab['cie']['modules'][248]['note'] = null;
+    $tab['cie']['modules'][248]['date'] = '';
+    $tab['cie']['modules'][259]['nom'] = "Développer des solutions ICT avec le machine learning";
+    $tab['cie']['modules'][259]['note'] = null;
+    $tab['cie']['modules'][259]['date'] = '';
+    $tab['cie']['modules'][293]['nom'] = "Créer et publier un site Web";
+    $tab['cie']['modules'][293]['note'] = 6;
     $tab['cie']['modules'][293]['date'] = '2022-11-10';
+    $tab['cie']['modules'][294]['nom'] = "Réaliser le front-end d'une application Web interactive";
+    $tab['cie']['modules'][294]['note'] = null;
+    $tab['cie']['modules'][294]['date'] = '';
+    $tab['cie']['modules'][295]['nom'] = "Réaliser le back-end pour des applications";
+    $tab['cie']['modules'][295]['note'] = null;
+    $tab['cie']['modules'][295]['date'] = '';
+    $tab['cie']['modules'][335]['nom'] = "Réaliser une application pour mobile";
+    $tab['cie']['modules'][335]['note'] = null;
+    $tab['cie']['modules'][335]['date'] = '';
 
     $tab['tpi']['desc'] = "TPI";
     $tab['tpi']['ponderation'] = 60;
@@ -106,7 +193,7 @@
             $nb_notes = 0;
             $somme_notes = 0;
 
-            foreach ($domaine['modules'] as $key => $module) {
+            foreach ($domaine['modules'] AS $key => $module) {
                 echo "<tr>";
                 if ($abr_domaine != "tpi") {
                     echo "<td>$key</td>";
@@ -150,7 +237,7 @@
         echo "</tr>";
 
     $note_globale = 0;
-    foreach ($tab as $key => $domaine) {
+    foreach ($tab AS $key => $domaine) {
         if ($key == "tpi" || $key == "comp_info") {
             echo "<tr>";
             echo "<td>".$domaine['desc']."</td>";
@@ -177,6 +264,96 @@ echo "</table>";
     echo "<pre>";
     print_r($tab);
     echo "</pre>";
+
+    /*I felt a funeral in my brain
+    and mourners to and fro
+    kept treading, treading till it seemed
+    that sense was breaking through
+
+    and when they all were seated
+    a service like a drum
+    kept beating beating till I thought
+    my mind was going numb
+
+    and then I heard them lift a box
+    and creak across my soul
+    with those same boots of lead again
+    then space began to toll
+
+    as all the heavens were a bell
+    and being but an ear
+    and I and silence some strange race
+    wrecked solitary here
+
+    and then a plank in reason broke
+    and I dropped down and down
+    and hit a world at every plunge
+    and finished knowing then*/
+
+    /*hope is the thing with feathers
+    that perches on the soul
+    and sings the tune without the words
+    and never stops at all
+
+    and sweetest in the gale is heard
+    and sore must be the storm
+    that could abash that little bird
+    that kept so many warm
+
+    I've heard it in the chillest lands
+    and on the strangest sea
+    yet never in extremity
+    it asked a crumb of me*/
+
+    /*I measure every grief I meet
+    with narrow probing eyes
+    I wonder if it weighs like mine
+    or has an easier size
+
+    I wonder if they bore it long
+    or did it just begin
+    I could not tell the date of mine
+    it feels so old a pain
+
+    I wonder if it hurts to live
+    and if they have to try
+    and whether could they choose between
+    it would not be to die
+
+    I note that some gone patient long
+    at length renew their smile
+    an imitation of a light
+    that has so little oil
+
+    I wonder if when years have passed
+    some thousands on the harm
+    that hurt them early such a lapse
+    could give them any balm
+
+    or would they go on aching still
+    through centuries of nerve
+    enlightened to a larger pain
+    in contrast with the love
+
+    the grieved are many i am told
+    there is the various cause
+    death is but one and comes but once
+    and only nails the eyes
+
+    there's grief of want and grief of cold
+    a sort they call despair
+    there's banishment from native eyes
+    in sight of native air
+
+    and though I may not guess the kind
+    correctly yet to me
+    a piercing comfort it affords
+    in passing calvary
+
+    to note that fashions of the cross
+    and how they're mostly worn
+    still fascinated to presume
+    that some are like my own*/
 ?>
 </body>
 </html>
