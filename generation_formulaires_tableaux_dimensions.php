343b58e7ebb31d5ea83dd4a493bef7de491755f4
Lucas Borter <lucas.borter@ceff.ch>
2023-09-05 11:48:43 +0200

    PHP 3.3.6 - Génération de formulaires avec tableaux a dimensions
    Utiliser des tableaux a plusieurs dimensions pour générer des formulaires

--- generation_formulaires_tableaux_dimensions.php
+++ generation_formulaires_tableaux_dimensions.php
@@ -6,20 +6,7 @@
 </head>
 <body>
     <?php
-    $carte['starter'][] =  "Salade verte";
-    $carte['starter'][] =  "Salade mêlée";
-    $carte['starter'][] =  "Soupe de courge";
-    $carte['starter'][] =  "Omelette aux fines herbes";
-    $carte['starter'][] =  "Omelette au fromage";
-    $carte['main'][] =  "Escalope de porc pannée";
-    $carte['main'][] =  "Entrecôte de boeuf de la région";
-    $carte['main'][] =  "Cordon bleu de veau à la tête de moine";
-    $carte['main'][] =  "Joue de boeuf braisée";
-    $carte['main'][] =  "Filet de sandre meunière";
-    $carte['desert'][] =  "Parfait glacé au chocolat noir";
-    $carte['desert'][] =  "Mousse au Damassons rouges";
-    $carte['desert'][] =  "Trilogie de crème brûlées";
-    $carte['desert'][] =  "Déclinaison de courge et chocolat noir";
+    require_once("carte.inc.php");
 
     echo "<form action='result.php' method='post'>";
         echo "<h1>Passez votre commande</h1>";
