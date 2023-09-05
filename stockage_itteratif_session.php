2ed5cc07459b6e69dc05d1ff1b40e2da06077648
Lucas Borter <lucas.borter@ceff.ch>
2023-09-05 11:54:18 +0200

    PHP 3.5.2 - Stockage itératif en session
    Première partie de l'exercice du stockage itératif en session

---
+++ stockage_itteratif_session.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Stockage ittératif en session</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<pre>
+<?php
+require_once("carte.inc.php");
+
+if (isset($_POST['save'])) {
+    //print_r($_POST);
+
+    $num_starter = $_POST['starter'];
+    $num_main = $_POST['main'];
+    $num_desert = $_POST['desert'];
+
+
+    $client = array("starter" => $num_starter, "main" => $num_main, "desert" => $num_desert);
+
+    $_SESSION['commande'][] = $client;
+
+    print_r($_SESSION);
+}
+?>
+</pre>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method='post'>
+    <h1>Passez votre commande</h1>
+    
+    <h3>Entrée</h3>
+    <select name='starter'>
+        <?php
+        $tab_starters = $carte['starter'];
+        foreach($tab_starters AS $key => $starter) {
+            echo "<option value='$key'>$starter</option>";
+        }
+        ?>
+    </select>
+
+    <h3>Plat principal</h3>
+    <select name='main'>
+        <?php
+        $tab_mains = $carte['main'];
+        foreach($tab_mains AS $key => $main) {
+            echo "<option value='$key'>$main</option>";
+        }
+        ?>
+    </select>
+
+    <h3>Dessert</h3>
+    <select name='desert'>
+        <?php
+        $tab_desert = $carte['desert'];
+        foreach($tab_desert AS $key => $desert) {
+            echo "<option value='$key'>$desert</option>";
+        }
+        ?>
+    </select>
+    <br><input type='submit' name='save' value='Commander'>
+</form>
+<br><a href="destroy.php">Destroy</a>
+</body>
+</html>
