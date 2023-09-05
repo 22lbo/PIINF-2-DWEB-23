513ff605815b7a157607b83f3db70dd18fd02d8c
Lucas Borter <lucas.borter@ceff.ch>
2023-09-05 11:48:03 +0200

    PHP 3.3.5 - Génération de formulaires avec tableaux
    Utiliser des tableaux pout générer des formulaires

--- generation_formulaires_tableaux.php
+++ generation_formulaires_tableaux.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8">
 </head>
 <body>
+    <h1>Commande client :</h1>
     <?php
     $carte[] =  "Escalope de porc pannée";
     $carte[] =  "Entrecôte de boeuf de la région";
     $carte[] =  "Cordon bleu de veau à la tête de moine";
     $carte[] =  "Joue de boeuf braisée";
     $carte[] =  "Filet de sandre meunière";
+    $carte[] =  "Fondue";
 
     echo "<form action='result.php' method='post'>";
-        echo "<h1>Passez votre commande</h1>";
+
         echo "<select name='repas'>";
             foreach($carte AS $key => $plat) {
                 echo "<option value='$key'>$plat</option>";
