e257058a80a0a0f4d32e397d7de35c10d007800f
Lucas Borter <lucas.borter@ceff.ch>
2023-09-05 13:43:47 +0200

    PHP - Page de résultats pour formulaires

--- result.php
+++ result.php
@@ -7,8 +7,45 @@
 <body>
 <?php
     echo "<pre>";
-        print_r($_POST);
-    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
 ?>
+<!-- generation de fomulaire avec des tableaux-->
+<!--    <h1>Commande client :</h1>
+    <pre>
+    --><?php
+/*        $carte[] =  "Escalope de porc pannée";
+        $carte[] =  "Entrecôte de boeuf de la région";
+        $carte[] =  "Cordon bleu de veau à la tête de moine";
+        $carte[] =  "Joue de boeuf braisée";
+        $carte[] =  "Filet de sandre meunière";
+        $carte[] =  "Fondue";
+
+
+        print_r($_POST);
+        echo "</pre>";
+
+        echo $carte[$_POST['repas']]
+    */?>
+
+<!-- generation de fomulaire avec des tableaux a multiples dimensions-->
+<!--    <h1>Commande client :</h1>
+    <pre>-->
+<!--    --><?php
+/*    require_once("carte.inc.php");
+
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+    $num_starter = $_POST['starter'];
+    echo "Entrée : ".$carte['starter'][$num_starter];
+
+    $num_main = $_POST['main'];
+    echo "<br>Plat principal : ".$carte['main'][$num_main];
+
+    $num_desert = $_POST['desert'];
+    echo "<br>Dessert : ".$carte['desert'][$num_desert];
+    */?>
 </body>
 </html>
