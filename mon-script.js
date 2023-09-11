77884491aaa436cd60979927713c0d0f3b2c8ba2
Lucas Borter <lucas.borter@ceff.ch>
2023-09-11 13:28:43 +0200

    JQuery - Script JS
    Script JS pour les exercices de JQuery

--- mon-script.js
+++ mon-script.js
@@ -1,3 +1,22 @@
+//Exercice 1
 $(function() {
     $('#texte').html('Hello World!')
+});
+
+//Exercice 2
+$(function() {
+    $('ul li').html('Green is not a creative color')
+});
+
+//Exercice 3
+$('.texte').each(function() {
+    $('#nb').html($('a').length)
+});
+
+//Exercice 4
+$('img').each(function(){
+   $('[name="master jedi"]').css('border', '4px solid blue');
+    $('[name!="jedi"]').css('height', '100px');
+   $('[name="jedi"]').css('height', '200px');
+    $('[name="master jedi"]').css('height', '250px');
 });
