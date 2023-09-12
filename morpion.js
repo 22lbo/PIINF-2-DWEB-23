8f60255177522b69758edab0f326290443771f1e
Lucas Borter <lucas.borter@ceff.ch>
2023-09-12 11:57:52 +0200

    JQuery - Exercice Morpion
    Fichier JS de l'exercice du Morpion.
    (New additions : Game over state when the table is full; Game restart (i.e. classes get removed) when game over state is reached)

--- morpion.js
+++ morpion.js
@@ -1,8 +1,10 @@
 var turn = 0;
+var full_cells = 0;
+var gameOver = false
 $(function(){
     $('td').click(function () {
         if ($(this).hasClass('symbole_0') || $(this).hasClass('symbole_1'))
-            $('#out').html('cell already full')
+            alert('illegal cell')
         else {
             if (turn == 0) {
                 $(this).addClass('symbole_0')
@@ -11,6 +13,21 @@ $(function(){
                 $(this).addClass('symbole_1')
                 turn = 0
             }
+        }
+        if ($(this).hasClass('symbole_0') || $(this).hasClass('symbole_1'))
+            full_cells++
+        if (full_cells == 9) {
+            alert('no more legal cells available')
+            gameOver = true
+        }
+        if(gameOver == true) {
+            alert('the game is over')
+            $('td').each(function(){
+                if ($('td').hasClass('symbole_0'))
+                    $('td').removeClass('symbole_0')
+                else
+                    $('td').removeClass('symbole_1')
+            })
         }
     })
 })
